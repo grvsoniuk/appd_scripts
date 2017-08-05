1f2e1e159fa1f5af7e884a8149ec8ff11911cb4f
Gaurav Soni <gaurav.soni@appdynamics.com>
2017-08-05 02:52:28 -0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@ mkdir appdynamics
 cd appdynamics
 sudo apt-get install git
 git clone https://grvsoniappd@bitbucket.org/appd_dashboard/appd_scripts.git
+git config --global user.email "gaurav.soni@appdynamics.com"
+git config --global user.name "Gaurav Soni"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-textsudo apt-get install libaio1
+sudo apt-get install sublime-text
+sudo apt-get install libaio1
 sudo apt-get install openssh-server 
 sudo apt install openjdk-8-jre-headless
 sudo apt-get install libaio1
